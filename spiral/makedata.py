c3aebb6e6b84a87209cd94afbe588046039b22f1
H. Watanabe <kaityo@users.sourceforge.jp>
2020-07-02 00:46:06 +0900

    updates makedata.py

--- spiral/makedata.py
+++ spiral/makedata.py
@@ -9,18 +9,19 @@ def get_data(step):
     data = np.zeros((L, L))
     for ix in range(L):
         for iy in range(L):
-            x = ix - L/2
-            y = iy - L/2
+            x = ix - L / 2
+            y = iy - L / 2
             phase = atan2(y, x)
             r = sqrt(x**2 + y**2)
-            data[ix][iy] = sin(r+phase+step)
+            data[ix][iy] = sin(r + phase + step)
     return data
 
 
 N = 16
 for i in range(N):
     filename = f"spiral{i:02d}.dat"
-    d = get_data(2.0*pi*i/N).reshape(L*L)
+    d = get_data(2.0 * pi * i / N).reshape(L * L)
     text = "\n".join([str(v) for v in d])
+    print(filename)
     with open(filename, "w") as f:
         f.write(text)
